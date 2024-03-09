1c50701f2af67f5f40c11e30a01f0bb71e533206
Waleed Saad <Waleed_saad36@yahoo.com>
2024-03-10 01:10:57 +0200

    add local file

---
+++ CustomCameraSDK/CustomCameraSDK.h
@@ -0,0 +1,18 @@
+//
+//  CustomCameraSDK.h
+//  CustomCameraSDK
+//
+//  Created by Waleed Saad on 10/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomCameraSDK.
+FOUNDATION_EXPORT double CustomCameraSDKVersionNumber;
+
+//! Project version string for CustomCameraSDK.
+FOUNDATION_EXPORT const unsigned char CustomCameraSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomCameraSDK/PublicHeader.h>
+
+
